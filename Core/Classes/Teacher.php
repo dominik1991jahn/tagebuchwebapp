1beeb338221c4553c86329e405be379ec56367d6
FearTheWhite <kisokelan@googlemail.com>
2014-06-26 12:04:45 +0200

    Klasse Teacher bearbeitet
    Müssen nur noch die nötigen Funktionen und der Konstruktor gefüllt
    werden

--- Core/Classes/Teacher.php
+++ Core/Classes/Teacher.php
@@ -9,11 +9,67 @@
 		private /*(string)*/ $name;
 		
 		//
+		// Constructor
+		//
+		public function Digikabu_Teacher()
+		{
+			
+		}
+		
+		//
 		// Methoden
 		//
+		
+		public function __get($name)
+		{
+			switch($name)
+			{
+				case "Kurz": return $this->getKurz(); break;
+				case "Name": return $this->getName(); break;
+			}
+		}
+		
+		public function __set($name,$value)
+		{
+			switch($name)
+			{
+				case "Kurz": $this->setKurz($value); break;
+				case "Name": $this->setName($value); break;
+			}
+		}
+		
+		  //
+		 // FUNCTIONS
+		//
+		
+		public static function doSomething() {}
 		
 		//
 		// Getter / Setter
 		//
+		
+		# Kurz
+		
+		public function getKurz()
+		{
+			return $this->kurz;
+		}
+		
+		public function setKurz(/*(string)*/ $kurz)
+		{
+			$this->kurz = $kurz;
+		}
+		
+		# Name
+		
+		public function getName()
+		{
+			return $this->name;
+		}
+		
+		public function setSubject(/*(string)*/ $name)
+		{
+			$this->name = $name;
+		}
 	}
 ?>
