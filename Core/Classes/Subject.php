3762df1e73fd3a9f871f1a035514e0ecb27433b5
black1256 <Alex_zilch@web.de>
2014-06-26 12:13:40 +0200

    Klasse Subject angefangen

--- Core/Classes/Subject.php
+++ Core/Classes/Subject.php
@@ -5,17 +5,56 @@
 	 
 	class Digikabu_Subject
 	{
-		   //
-		  // ATTRIBUTES
-		 //
-		 	   
-		   //
-		  // METHODS
-		 //
+		  //
+		 // ATTRIBUTES
+		//
+		 
+		private /*(string)*/ $name;
+		private /*(string)*/ $description;
+			  
+		  //
+		 // CONSTRUCTORS
+		//		
+		
+		public function Digikabu_Subject()
+		{
+		}
+
+		 	  
+		  //
+		 // METHODS
+		//
+		 
+		 
+		  //
+		 // FUNCTIONS
+		//		  
 		  
-		   //
-		  // GETTERS & SETTERS
-		 //
+		  
+		  //
+		 // GETTERS & SETTERS
+		//
+		
+		public function getName()
+		{
+			return $this->name;
+		}
+		
+		public function setName(/*(string)*/ $name)
+		{
+			return $this->name = $name;
+		}
+		
+		public function getDescription()
+		{
+			return $this->description;
+		}
+		
+		public function setDescription(/*(string)*/ $description)
+		{
+			$this->description = $description;
+		}
+		
 		public function __toString()
 		{
 			return "I'm a subject";
