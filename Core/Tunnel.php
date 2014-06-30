82cc572af7d8643493a22cebdd05dff28cbc0967
dominik1991jahn <dominik1991jahn@gmail.com>
2014-06-30 12:54:52 +0200

    Tunnel.php und Configuration.xml angelegt

---
+++ Core/Tunnel.php
@@ -0,0 +1,45 @@
+<?php
+	class Tunnel
+	{
+		  //
+		 // ATTRIBUTES
+		//
+		
+		private /*(string)*/ $username;
+		private /*(string)*/ $password;
+		
+		  //
+		 // CONSTRUCTOR
+		//
+		
+		public function Tunnel($username, $password)
+		{
+			$this->username = $username;
+			$this->password = $password;
+		}
+		
+		  //
+		 // METHODS
+		//
+		
+		public function GetSchedule()
+		{
+			
+		}
+		
+		public function GetClassList()
+		{
+			
+		}
+		
+		public function GetTeacherList()
+		{
+			
+		}
+		
+		public function GetSubjectList()
+		{
+			
+		}
+	}
+?>
