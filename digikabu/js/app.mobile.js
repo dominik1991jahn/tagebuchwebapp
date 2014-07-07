7af670d7a618e78e22ce54cb288c8ff9ab35c32f
AlexZilch <Alex_zilch@web.de>
2014-07-07 13:35:08 +0200

    test

--- digikabu/js/app.mobile.js
+++ digikabu/js/app.mobile.js
@@ -9,7 +9,7 @@ function FillClassList()
 			$.each(response, function(key, value)
 			{
 				$("#ClassList").append("<option>"+value.Name+"</option>");
-			})
+			});
 			
 			$("#ClassTeacherList").selectmenu('refresh');
 		});
@@ -26,13 +26,47 @@ function FillTeacherList()
 			$.each(response, function(key, value)
 			{
 				$("#TeacherList").append("<option value=\""+value.Abbreviation+"\">["+value.Abbreviation+"] "+value.Name+"</option>");
-			})
+			});
 			
 			$("#ClassTeacherList").selectmenu('refresh');
 		});
 }
 
+function LoadScheduleDataForDate(classcode, startdate)
+{
+	var url = "request.php?/Schedule/" + classcode + "-" + startdate + ".xml";
+	
+	$.getJSON(
+		url,
+		function(response)
+		{
+			
+		}
+	)
+}
+
+/*
+ * data = JSON-Data
+ */
+function CreateScheduleForDay(data)
+{
+	date = Date.parse(data.Date);
+	pageid = "schedule-"+date;
+	
+	page = document.createElement("div");
+	page = $(page);
+	
+	page.attr('data-role','page').attr('id',pageid);
+	
+	$(document).append(page);
+}
+
+/*
+ * Auto-Start
+ */
+
 $(function() {
 	FillClassList();
 	FillTeacherList();
+	CreateScheduleForDay({Date:'2014-07-08'});
 });
