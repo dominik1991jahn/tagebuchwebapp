ba8d648924f7965c82bd2785f75f36f0c315114b
FelixHollmann <felix.hollmann95@gmx.net>
2014-07-16 11:54:40 +0200

    fix

--- js/app.mobile.js
+++ js/app.mobile.js
@@ -1,69 +1,98 @@
-var schedule = new Array();
-
-function FillClassList()
-{
-	var url = "request.php?/Class";
+	var schedule = new Array();
 	
-	$.getJSON(
-		url,
-		function(response)
-		{
-			$.each(response, function(key, value)
+	function FillClassList()
+	{
+		var url = "request.php?/Class";
+		
+		$.getJSON(
+			url,
+			function(response)
 			{
-				$("#ClassList").append("<option>"+value.Name+"</option>");
-			});
-			
-			$("#ClassTeacherList").selectmenu('refresh');
-		});
-}
-
-function FillTeacherList()
-{
-	var url = "request.php?/Teacher";
-	
-	$.getJSON(
-		url,
-		function(response)
-		{
-			$.each(response, function(key, value)
-			{
-				$("#TeacherList").append("<option value=\""+value.Abbreviation+"\">["+value.Abbreviation+"] "+value.Name+"</option>");
-			});
-			
-			$("#ClassTeacherList").selectmenu('refresh');
-		});
-}
-
-function LoadScheduleDataForDate(classcode, startdate)
-{
-	var url = "request.php?/Schedule/Class/" + classcode + "-" + startdate;
-	
-	$.getJSON(
-		url,
-		function(response)
-		{
-			// Weeks
-			$.each(response, function(key, value)
-			{
-				for(d = 0; d < value.Days.length; d++)
+				$.each(response, function(key, value)
 				{
-					CreateScheduleForDay(value.Days[d]);
-					break; // REMOVE! Only for testing!
-				}
+					$("#ClassList").append("<option>"+value.Name+"</option>");
+				});
+				
+				$("#ClassTeacherList").selectmenu('refresh');
 			});
+	}
+	
+	function FillTeacherList()
+	{
+		var url = "request.php?/Teacher";
+		
+		$.getJSON(
+			url,
+			function(response)
+			{
+				$.each(response, function(key, value)
+				{
+					$("#TeacherList").append("<option value=\""+value.Abbreviation+"\">["+value.Abbreviation+"] "+value.Name+"</option>");
+				});
+				
+				$("#ClassTeacherList").selectmenu('refresh');
+			});
+	}
+	
+	function LoadScheduleDataForDate(classcode, startdate)
+	{
+		var url = "request.php?/Schedule/Class/" + classcode + "-" + startdate;
+		
+		$.ajax({
+			type: "GET",
+			url: url,
+			dataType: 'json',
+			success: function(response) {
+				$.each(response, function(key, value)
+				{
+					date = value.Date;
+					schedule[date]=value;
+					//CreateScheduleForDay(value);
+					//break; // REMOVE! Only for testing!
+				});
+			},
+			async: false
+		});
+	}
+	
+	/*
+	 * data = JSON-Data
+	 */
+	function CreateScheduleForDay(data)
+	{
+		currentDate=new Date(data.Date);
+		
+		prevdate = new Date(currentDate.getTime()-86400000);
+	 	prevpage="schedule-"+DateToUTC(prevdate);
+	 	
+	 	nextdate = new Date(currentDate.getTime()+86400000);
+	 	nextpage="schedule-"+DateToUTC(nextdate);
+		
+		pageid = "schedule-"+DateToUTC(currentDate);//data.Date.replace(/-/g,'');
+		
+		page = document.createElement("div");
+		page = $(page);
+		
+		page.attr('data-role','page').attr('id',pageid);
+		page.attr('data-prev',prevpage);
+		page.attr('data-next',nextpage);
+		
+		header = document.createElement('div');
+		header = $(header);
+		
+		header.attr('data-role','header').append('<h1>Digikabu.App</h1>');
+		
+		footer = CreateFooter(currentDate);
+		
+		content = document.createElement('div');
+		content = $(content);
+		content.attr('data-role','content');
+		
+		for(p = 0; p < data.Periods.length; p++)
+		{
+			lesson = CreateLesson(data.Periods[p]);
+			content.append(lesson)
 		}
-<<<<<<< HEAD
-	)
-}
-
-/*
- * data = JSON-Data
- */
-function CreateScheduleForDay(data)
-{
-	schedule[data.Date]=data;
-	currentDate=new Date(data.Date);
-=======
 		
 		page.append(header);
 		page.append(content);
@@ -194,110 +223,61 @@ function CreateScheduleForDay(data)
 		</div>
 		*/
 	}
->>>>>>> bb861b11e4330239b15cbaf7e88f5ab76505a671
 	
-	pageid = "schedule-"+data.Date.replace(/-/g,'');
-	
-	page = document.createElement("div");
-	page = $(page);
-	
-	page.attr('data-role','page').attr('id',pageid);
-	
-	header = document.createElement('div');
-	header = $(header);
-	
-	header.attr('data-role','header').append('<h1>Digikabu.App</h1>');
-	
-	footer = CreateFooter(currentDate);
-	
-	content = document.createElement('div');
-	content = $(content);
-	
-	content.attr('data-role','content');
-	content.html('<p>Hallo</p>');
-	
-	page.append(header);
-	page.append(content);
-	page.append(footer);
-	
-	page.page();
-	page.appendTo($.mobile.pageContainer);
-	
-	alert(page.html());
-	$.mobile.changePage("#"+pageid);
-	
-	
-}
- 
- function CreateFooter(currentDate)
- {
- 	prevdate = new Date(currentDate-86400000);
- 	prevdate_id=DateToUTC(prevdate);
- 	
- 	nextdate = new Date(currentDate+86400000);
- 	nextdate_id=DateToUTC(nextdate);
- 	
- 	nextID="schedule-"+nextdate_id;
- 	prevID="schedule-"+prevdate_id;
- 	footer=$(document.createElement("div")); 	
- 	footer.attr("data-role","footer").attr("data-position","fixed");
- 	
-	 	datnavbar=$(document.createElement("div"));	 	
-	 	datnavbar.attr("data-role","navbar");
-	 	
-		 	datnavbarlist=$(document.createElement("ul"));
-		 		datnavbarprev=$(document.createElement("li"));
-		 			datnavbarprev.append("<a></a>").attr("href","#"+prevID).attr("data-role","button").html(prevdate.getUTCDate());
-		 		datnavbarnext=$(document.createElement("li"));
-		 			datnavbarnext.append("<a></a>").attr("href","#"+nextID).attr("data-role","button").html(nextdate.getUTCDate());
- 			datnavbarlist.append(datnavbarprev);
- 			datnavbarlist.append(datnavbarnext);
- 		datnavbar.append(datnavbarlist);
- 	footer.append(datnavbar);
- 	return footer;
- 	/*
- 	<div data-role="footer" data-position="fixed">
-				<div data-role="navbar">
-					<ul>
-						<li><a href="#Previous" data-role="button" data-iconpos="left">Montag (30.06)</a></li>
-						<li><a href="#schedule-20140707">Dienstag (01.07)</a></li>
-						<li><a href="#Next" data-role="button" data-iconpos="right">Mittwoch (02.07)</a></li>
-					</ul>
-				</div>
-				
-				<div style="text-align:center">
-					<select data-native-menu="false" id="ClassTeacherList">
-						<option data-placeholder="true">Klasse oder Lehrer ausw&auml;hlen</option>
-						<optgroup id="ClassList" label="Klassen"></optgroup>
-						<optgroup id="TeacherList" label="Lehrer"></optgroup>
-					</select>
-				</div>
-			</div>
-			*/
- }
-/*
- * Auto-Start
- */
-
-$(function() {
-	FillClassList();
-	FillTeacherList();
-	LoadScheduleDataForDate("IT11a","2014-07-07");
-});
-
-function DateToUTC(date)
-{
-	utc = date.getFullYear()+"-";
-	month=date.getMonth();
-	day=date.getDate();
-	if(date.getMonth()<10)
+	function DateToUTC(date)
 	{
-		month="0"+month;
+		utc = date.getUTCFullYear()+"-";
+		
+		month=date.getUTCMonth()+1;
+		day=date.getUTCDate();
+		
+		if(month<10)
+		{
+			month="0"+month;
+		}
+		if(day<10)
+		{
+			day = "0"+day;
+		}
+		utc+=month+"-"+day;
+		return utc;
 	}
-	if(date.getDate()<10)
+	
+	function DayOfWeekToName(day)
 	{
-		day = "0"+day;
+		days = ["Montag","Dienstag","Mittwoch","Donnerstag","Freitag","Samstag","Sonntag"];
+		
+		return days[day];
 	}
-	utc+=month+"-"+day;
-	return utc;
-}
+	
+	function Zero(num)
+	{
+		if(num < 10) num = "0"+num;
+		
+		return num;
+	}
+	
+	function switchToPage(date)
+	{
+		if(!(date in schedule))
+		{
+			alert(unescape("F%FCr dieses Datum sind keine Daten vorhanden!"));
+		}
+		else
+		{
+			CreateScheduleForDay(schedule[date]);
+			$.mobile.changePage("#schedule-"+date);
+		}
+	}
+
+	
+	/*
+	 * Auto-Start
+	 */
+	
+	$(function(){
+		currentDate = DateToUTC(new Date());
+		LoadScheduleDataForDate("bfi11a",currentDate);
+		
+		switchToPage(currentDate);
+	});
