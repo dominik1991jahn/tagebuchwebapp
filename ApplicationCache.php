96ae77fc3a63610b88e0875f4b8bfbbaf519aca6
dominik1991jahn <dominik1991jahn@gmail.com>
2014-07-23 20:50:23 +0200

    Vergessen, die ApplicationCache.php wieder einzubinden

--- ApplicationCache.php
+++ ApplicationCache.php
@@ -1,41 +1,30 @@
 <?php
 	header("Content-Type: text/cache-manifest");
+	
+	$files = array_merge(glob("js/*.*"),glob("css/*.*"),glob("img/*.*"),glob("img/icons-png/*.*"),glob("img/icons-svg/*.*"));
+	$files[] = "index.html";
+	
+	$lastchange = 0;
+	
+	foreach($files as $file)
+	{
+		$modified = filemtime($file);
+		
+		if($lastchange < $modified)
+		{
+			$lastchange = $modified;
+		}
+	}
 ?>
 CACHE MANIFEST
 
 CACHE:
-notfound.html
-
-NETWORK:
-*
 <?php
-	echo "#".microtime();
-	die();
+foreach($files as $file)
+{
+	echo $file."\n";
+}
 ?>
-
-CACHE:
-css/app.moblie.css
-css/digikabuThemeRoller.css
-css/jquery.mobile.icons.min.css
-css/jquery-mobile.css
-css/jquery-mobile-datepicker.css
-js/app.mobile.autostart.js
-js/app.mobile.cookie.js
-js/app.mobile.events.js
-js/app.mobile.js
-js/app.mobile.login.js
-js/app.mobile.schedule.js
-js/app.mobile.tools.js
-js/jquery.js
-js/jquery-browser.js
-js/jquery-datepicker.js
-js/jquery-mobile.js
-js/jquery-mobile-datepicker.js
-img/ajax-loader.gif
-img/schraffur11.png
-img/schraffur21.png
-img/schraffur81.png
-index.html
 
 NETWORK:
 *
@@ -43,5 +32,5 @@ NETWORK:
 <?php
 	//if anything has changed, add this comment:
 	//echo "#".$dateOfLastChange;
-	echo "#".microtime();
+	echo "#".date("Y-m-d H:i:s",$lastchange);
 ?>
